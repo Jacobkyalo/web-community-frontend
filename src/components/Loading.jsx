583343db8d087de066d0ecd42e728be3a0b4c0f3
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-10 00:22:24 +0300

    added loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+export const Loading = () => {
+  return (
+    <div className="container">
+      <div className="card-wrapper">
+        <div className="card">
+          <p>Please wait...</p>
+        </div>
+      </div>
+    </div>
+  );
+};
