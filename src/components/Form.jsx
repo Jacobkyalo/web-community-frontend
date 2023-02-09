57646f4be928d039ac0dc04a024fef018a328711
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-02-10 00:22:58 +0300

    updated form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
+import { Loading } from "./Loading";
 
 export const Form = () => {
+  const [loading, setLoading] = useState(false);
   const [state, setState] = useState({
     firstname: "",
     lastname: "",
@@ -20,69 +22,74 @@ export const Form = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await Axios.post(
+    setLoading(true);
+    await Axios.post(
       "https://web-community-attendance-backend-production.up.railway.app/attendance",
       state
     );
-    console.log(res);
+    setLoading(false);
     navigate("/confirm");
   };
 
   return (
     <div className="container">
-      <div className="form-wrapper">
-        <h2 className="form-header">
-          Please fill the following fields to submit your attendance!
-        </h2>
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="firstname">Firstname</label>
-              <input
-                type="text"
-                placeholder="Firstname"
-                name="firstname"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="lastname">Lastname</label>
-              <input
-                type="text"
-                placeholder="Lastname"
-                name="lastname"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="year">Year of Study</label>
-              <input
-                type="number"
-                placeholder="Year of Study"
-                name="year"
-                min="1"
-                max="4"
-                required
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea
-                name="message"
-                cols="30"
-                rows="3"
-                placeholder="Message about the today's session"
-                required
-                onChange={handleChange}
-              />
-            </div>
-            <input type="submit" value="Submit Attendance" />
-          </form>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="form-wrapper">
+          <h2 className="form-header">
+            Please fill the following fields to submit your attendance!
+          </h2>
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstname">Firstname</label>
+                <input
+                  type="text"
+                  placeholder="Firstname"
+                  name="firstname"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastname">Lastname</label>
+                <input
+                  type="text"
+                  placeholder="Lastname"
+                  name="lastname"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="year">Year of Study</label>
+                <input
+                  type="number"
+                  placeholder="Year of Study"
+                  name="year"
+                  min="1"
+                  max="4"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  name="message"
+                  cols="30"
+                  rows="3"
+                  placeholder="Message about the today's session"
+                  required
+                  onChange={handleChange}
+                />
+              </div>
+              <input type="submit" value="Submit Attendance" />
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
