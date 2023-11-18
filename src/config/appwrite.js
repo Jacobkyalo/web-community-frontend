1ca0fa0322ec0184de073b1c0ade0a4e6acda7fc
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-11-18 13:46:41 +0300

    updated appwrite config

--- src/config/appwrite.js
+++ src/config/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
 const client = new Client();
 
@@ -7,5 +7,7 @@ client
   .setProject(import.meta.env.VITE_PROJECT_ID);
 
 export const databases = new Databases(client);
+export const storage = new Storage(client);
 export const DB_ID = import.meta.env.VITE_DB_ID;
 export const COLL_ID = import.meta.env.VITE_COLL_ID;
+export const BUCKET_ID = import.meta.env.VITE_BUCKET_ID;
